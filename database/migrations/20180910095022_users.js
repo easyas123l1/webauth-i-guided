09adbeea131b98741cbcd565e59c3b95db5a46d6
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-12-16 13:11:33 -0600

    after lecture

--- database/migrations/20180910095022_users.js
+++ database/migrations/20180910095022_users.js
@@ -1,15 +1,24 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
+  return knex.schema.createTable("users", users => {
     users.increments();
 
     users
-      .string('username', 128)
+      .string("username", 128)
       .notNullable()
       .unique();
-    users.string('password', 128).notNullable();
+    users.string("password", 128).notNullable();
+
+    users
+      .integer("role_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("roles")
+      .onUpdate("CASCADE")
+      .onDelete("RESTRICT");
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists("users");
 };
